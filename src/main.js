f850ca1045ed88fbbf0782ad638b37127b3f791b
OFFICE\yueqin.zuo <yueqin.zuo@freemud.cn>
2018-07-06 14:19:24 +0800

    添加判断用户是否登录

--- src/main.js
+++ src/main.js
@@ -8,10 +8,23 @@ import store from './store'
 Vue.config.productionTip = false
 
 router.beforeEach((to, from, next) => {
-  to.meta.pageChangeState = window.pageChangeState
-  document.title = !!to.meta.title ? to.meta.title : ''
-  router.isShowNavbar = to.meta.isShowNavbar
-  next()
+  /*判断用户是否登录*/
+  if(window.localStorage.getItem('token')){
+    to.meta.pageChangeState = window.pageChangeState
+    document.title = !!to.meta.title ? to.meta.title : ''//即将进入页面的标题
+    router.isShowNavbar = to.meta.isShowNavbar
+    next()
+  }else{
+    //防止死循环
+    if(to.path=='/login'){
+      next()
+    }else{
+      next('/login')
+    }
+  }
+})
+router.afterEach((to,from,next)=>{
+  //window.scrollTo(0,0);
 })
 
 /* eslint-disable no-new */
